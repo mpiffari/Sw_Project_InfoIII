f3b168db5bd66764e1c9d7eab41fd3233cfff891
Piffo <michele.piffari@gmail.com>
2019-08-21 13:20:45 +0200

    Add book request type

--- JavaFiles/ServerRequestHandler/src/main/java/requestManager/ComputeRequest.java
+++ JavaFiles/ServerRequestHandler/src/main/java/requestManager/ComputeRequest.java
@@ -5,15 +5,18 @@ import book.Book;
 public class ComputeRequest implements ProcessRequest{
 	
 	/**
-	 * @param msg: is a string somthing like this request-Type:<number that specifies the quest>;<other data>
+	 * @param msg: is a string something like this 
+	 * <request-Type>: number that specifies type of request;
+	 * <other data>
 	 */
 	public void process(String msg, String username) {
 		int i = msg.indexOf(";", 0);
-		String str = msg.substring(0, i);
 		int j = msg.indexOf(":", 0);
+		RequestType requestType = RequestType.valueOf(Integer.parseInt(msg.substring(0, i).substring(j+1)));
 		
-		switch(Integer.parseInt(str.substring(j+1))) {
-			case 0:
+		
+		switch(requestType) {
+			case BOOK_REGISTRATION:
 				Book b = new Book(msg.substring(i + 1));
 				b.setProprietario(username);
 				//TODO: remove the following line
@@ -21,9 +24,9 @@ public class ComputeRequest implements ProcessRequest{
 				
 				Communication.getInstance().send(username, "requestType: 0; result: " + b.insert());
 				break;
-			case 1:
+			case BOOK_RESERVATION:
 				break;
-			case 2:
+			default:
 				break;
 		}
 		
