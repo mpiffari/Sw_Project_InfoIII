6fac4b22bd0cd88c664faf5a88ccef1c221c12f4
stefanovilla96 <villa.stefano96@gmail.com>
2019-09-10 15:40:00 +0200

    add db schema

--- JavaFiles/ServerRequestHandler/src/main/java/user/UserManager.java
+++ JavaFiles/ServerRequestHandler/src/main/java/user/UserManager.java
@@ -3,9 +3,10 @@ package user;
 import java.util.ArrayList;
 
 import book.Book;
+import dataManager.Localization;
 
 public interface UserManager {
 	public double computeDistance(User o);
-	public double computeDistance(UserLocalizationInfo o);
+	public double computeDistance(Localization o);
 	public ArrayList<Book> getChasingBooks();
 }
